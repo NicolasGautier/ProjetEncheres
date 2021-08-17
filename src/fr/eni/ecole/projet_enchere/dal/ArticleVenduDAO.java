ee1403e45b25d07b9fc95474e0f61b58c5152d21
Nicolas Gautier <nicolas.gautier2021@campus-eni.fr>
2021-08-17 10:30:00 +0200

    commit

--- src/fr/eni/ecole/projet_enchere/dal/ArticleVenduDAO.java
+++ src/fr/eni/ecole/projet_enchere/dal/ArticleVenduDAO.java
@@ -1,5 +1,21 @@
 package fr.eni.ecole.projet_enchere.dal;
 
-public class ArticleVenduDAO {
+import java.util.List;
 
+import fr.eni.ecole.projet_enchere.bo.ArticleVendu;
+import fr.eni.ecole.projet_enchere.bo.Utilisateur;
+
+public interface ArticleVenduDAO {
+
+	public void insert(ArticleVendu articlevendu) throws DALException;
+
+	public void update(ArticleVendu articlevendu) throws DALException;
+
+	public void delete(Integer id) throws DALException;
+	
+	public List<Utilisateur> selectAll() throws DALException;
+	
+	public Utilisateur selectById(Integer id) throws DALException;
+	
+	
 }
