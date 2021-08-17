e6c7e47e3f6c91346dfa6d5fb50348be6347e521
fanch cherhal <fcherhal2021@21SE24-B7NZG5J.ad.campus-eni.fr>
2021-08-17 11:53:34 +0200

    jsp insertion

---
+++ src/fr/eni/ecole/projet_enchere/bll/UtilisateurManagerSingl.java
@@ -0,0 +1,11 @@
+package fr.eni.ecole.projet_enchere.bll;
+
+public class UtilisateurManagerSingl {
+private static UtilisateurManager instance;
+public static UtilisateurManager getInstance() {
+	if (instance ==null) {
+		instance = new UtilisateurManagerImpl();
+	}
+	return instance;
+}
+}
