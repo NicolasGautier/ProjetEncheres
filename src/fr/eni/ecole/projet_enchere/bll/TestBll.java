0b8866282e61c13ebabf63b431f0acf50a03f8a3
Nicolas Gautier <nicolas.gautier2021@campus-eni.fr>
2021-08-18 10:15:14 +0200

    Modif test bll

--- src/fr/eni/ecole/projet_enchere/bll/TestBll.java
+++ src/fr/eni/ecole/projet_enchere/bll/TestBll.java
@@ -16,17 +16,17 @@ public class TestBll {
 		Utilisateur user3 = new Utilisateur(3, "stephlapoisse", "LeChat", "Stephane", "steph39@omail.com", "0652525252", "rue de l'échelle", "44500","machecoul", "lechat456", 0, false);
 
 		
-		manager.insert(user1);
-		manager.insert(user2);
-		manager.insert(user3);
+		manager.addUtilisateur(user1);
+		manager.addUtilisateur(user2);
+		manager.addUtilisateur(user3);
 		
-		System.out.println(manager.selectAll());
+		System.out.println(manager.getAllUtilisateurs());
 		
 //		System.out.println("TEST 2 : INSERTION D'un user avec le même login et mail");
 //		manager.insert(new Utilisateur(4,"jaja", "Dupont", "Jeannine", "jaja43@omail.com", "0650505050", "rue du port", "44400","reze", "123456", 0, false));
 //		
 		System.out.println("TEST 3 : INSERTION D'un user avec un login non alphanumérique");
-		manager.insert(new Utilisateur(5,"§/%/", "DOpont", "JeanninOe", "jajOa43@omail.com", "06505505050", "rOue du port", "40400","rezOe", "1234567", 0, false));
+		manager.addUtilisateur(new Utilisateur(5,"§/%/", "DOpont", "JeanninOe", "jajOa43@omail.com", "06505505050", "rOue du port", "40400","rezOe", "1234567", 0, false));
 //		
 //		System.out.println("TEST 4 : verif de l'existence du login et du mot de passe en base de donnée");
 //		System.out.println(manager.logAndPassChecked(user3));
